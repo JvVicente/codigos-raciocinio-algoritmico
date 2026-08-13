2fe35c5764112a8b415d3d408a6724f0eea8b1a9
João Vitor Vicente <112700592+JvVicente@users.noreply.github.com>
2026-08-13 19:15:13 -0300

    Lista Modularização

---
+++ Lista_Modularizacao/Exer_1/conversor.py
@@ -0,0 +1,6 @@
+def conversaoMetros():
+    metros = float(input("Digite quantos metro você deseja converter para centimetros: "))
+    print(f"A conversão de metros para centimetros é: {metros * 100}")
+
+def conversorCelsius():
+    celsius = float 
